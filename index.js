2b4479258c41e6bed165ca4bcf13bc6f1a9b7adf
sajjadhossain <sajjadhossain0756@gmail.com>
2024-12-23 23:25:49 +0600

    insert items in mongodb done

--- index.js
+++ index.js
@@ -27,7 +27,16 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+    const database = client.db("lost&foundDB");
+    const lostAndFoundCollection = database.collection("lostAndFoundItems");
+
+    app.post('/allItems',async(req,res)=>{
+        const lostAndFoundItems = req.body;
+        const result = await lostAndFoundCollection.insertOne(lostAndFoundItems);
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
