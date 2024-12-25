0f06adedeb3c3ed325a22e1e3db5577dba4aaa3f
sajjadhossain <sajjadhossain0756@gmail.com>
2024-12-25 22:38:44 +0600

    get myItems by email done

--- index.js
+++ index.js
@@ -33,30 +33,41 @@ async function run() {
     const recoveredCollection = database.collection("recoveredItems");
 
     // get all items mongodb to server
-    app.get('/allItems',async(req,res)=>{
-        const result = await lostAndFoundCollection.find().toArray()
-        res.send(result)
+    app.get('/allItems', async (req, res) => {
+      const result = await lostAndFoundCollection.find().toArray()
+      res.send(result)
     })
     // get one items by id from mongodb to server
-    app.get('/allItems/:id',async(req,res)=>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const result = await lostAndFoundCollection.findOne(query)
-        res.send(result)
+    app.get('/allItems/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await lostAndFoundCollection.findOne(query)
+      res.send(result)
     })
+    // get my add items by user email 
+    app.get('/allItems/myItems/:email', async (req, res) => {
+
+        const email = req.params.email;
+
+        const query = { userEmail: email};
+        const cursor = lostAndFoundCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+    });
+
     // insert one item client to db;
-    app.post('/allItems',async(req,res)=>{
-        const lostAndFoundItems = req.body;
-        const result = await lostAndFoundCollection.insertOne(lostAndFoundItems);
-        res.send(result)
+    app.post('/allItems', async (req, res) => {
+      const lostAndFoundItems = req.body;
+      const result = await lostAndFoundCollection.insertOne(lostAndFoundItems);
+      res.send(result)
     })
 
     // recovered collection start here
     // insert one items client to db;
-    app.post('/recoveredItems',async(req,res)=>{
-        const recoveredItems = req.body;
-        const result = await recoveredCollection.insertOne(recoveredItems);
-        res.send(result)
+    app.post('/recoveredItems', async (req, res) => {
+      const recoveredItems = req.body;
+      const result = await recoveredCollection.insertOne(recoveredItems);
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
